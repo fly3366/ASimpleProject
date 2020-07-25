b0f05e2fea210e4ed26faaf9497364fa2f67337a
fly3366 <feiliuxz@hotmail.com>
2020-07-25 15:30:15 +0800

    2333

--- back_end/app/src/main/java/projectapp/runtime/Main.java
+++ back_end/app/src/main/java/projectapp/runtime/Main.java
@@ -2,12 +2,33 @@ package projectapp.runtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude =
+        {DataSourceAutoConfiguration.class,
+                MongoAutoConfiguration.class,
+                RedisAutoConfiguration.class,
+                SecurityAutoConfiguration.class,
+                KafkaAutoConfiguration.class})
+@RestController
+@RequestMapping(path = "/")
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @RequestMapping(path = "/")
+    public String hello() {
+        return "2333";
     }
 
 }
